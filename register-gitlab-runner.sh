b7a397b09006aa7dbed5179771cf6a9697335b3e
Jesper Lehtinen <jsprlehtinen@gmail.com>
2020-06-21 18:20:00 +0200

    add clone url to resolve gitlab on docker network change internal listen port to 80

--- register-gitlab-runner.sh
+++ register-gitlab-runner.sh
@@ -4,7 +4,9 @@ docker exec -it gitlab-runner-1 \
     --registration-token "_GGx_qEiZQQNYi5YiCLx" \
     --locked=false \
     --description "gitlab-runner-1" \
-    --url "http://gitlab:8888" \
+    --url "http://gitlab" \
     --executor docker \
     --docker-image alpine:latest \
-    --docker-volumes "/var/run/docker.sock:/var/run/docker.sock"
+    --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
+    --docker-network-mode "gitlab-docker_continuous-network" \
+    --clone-url "http://gitlab"
